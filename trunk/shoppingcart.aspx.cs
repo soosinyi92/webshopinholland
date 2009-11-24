3a1d65f0e84a1526f8a90360f02c00527c54441b
DimitarRosenovKolev@hotmail.com <DimitarRosenovKolev@hotmail.com@f7cd67c8-b1e6-11de-9cd7-678226503d6e>
2009-11-24 21:23:03 +0000

    > For Phill look it.

--- trunk/shoppingcart.aspx.cs
+++ trunk/shoppingcart.aspx.cs
@@ -16,20 +16,20 @@ public partial class shoppingcart : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        Dictionary<Int64, ShoppingCart.Item> cart = Profile.User.ShoppingCart.getItems();
+        List<ShoppingCart.Item> cart = Profile.User.ShoppingCart.getItems();
         rptShoppingCart.DataSource = cart;
         rptShoppingCart.DataBind();
     }
 
     protected void lbtnDelete_Click(object sender, CommandEventArgs e)
     {
-        Dictionary<Int64, ShoppingCart.Item> cart = Profile.User.ShoppingCart.getItems();
+        List<ShoppingCart.Item> cart = Profile.User.ShoppingCart.getItems();
 
         LinkButton lnk = sender as LinkButton;
 
         int index = ((RepeaterItem)lnk.NamingContainer).ItemIndex;
 
-        cart.Remove(Int64.Parse(e.CommandArgument.ToString()));
+        //cart.Remove(Int64.Parse(e.CommandArgument.ToString()));
 
         rptShoppingCart.DataBind();
     }
